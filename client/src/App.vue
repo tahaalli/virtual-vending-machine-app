752b3f631739224639b57458a6f1b0517a74bcf3
tahaalli <tahaalli@hotmail.com>
2021-10-14 00:14:23 -0400

    Dev commit

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Soda
+      :data="{ product_id: 'cola', product_description: 'Some text here' }"
+    />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Soda from "./components/Soda.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Soda,
+  },
+};
+</script>
---
+++ client/src/components/Soda.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card" style="width: 10rem">
+    <img
+      v-bind:src="getImgUrl(data.product_id)"
+      alt="Soda Image"
+      class="card-img-top"
+    />
+    <div class="card-body">
+      <span class="card-text">{{ data.product_description }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Soda",
+  props: {
+    data: Object,
+  },
+  methods: {
+    getImgUrl(product_id) {
+      return require("@/assets/" + product_id + ".png");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
